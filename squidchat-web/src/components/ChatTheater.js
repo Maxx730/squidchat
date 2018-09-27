9cade13d06c48d126aaf11a8935bef79beaf731f
ddcjkinghorn <john.kinghorn@coxautoinc.com>
2018-09-27 16:53:19 -0400

    update

--- squidchat-web/src/components/ChatTheater.js
+++ squidchat-web/src/components/ChatTheater.js
@@ -1,13 +1,15 @@
 import React,{ Component } from 'react'
 import Paper from '@material-ui/core/Paper'
 
+import TheaterControls from './TheaterControls'
 import './css/ChatTheater.css'
 
 class ChatTheater extends Component{
     render(){
         return(
             <Paper className="FloatingTheater">
-                <iframe width="200" height="200" src="https://www.youtube.com/embed/H_Z9orDS84c" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                <iframe width="280" src="https://www.youtube.com/embed/H_Z9orDS84c" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                <TheaterControls/>
             </Paper>
         )
     }
---
+++ squidchat-web/src/components/TheaterControls.js
@@ -0,0 +1,13 @@
+import React,{ Component } from 'react'
+
+class TheaterControls extends Component{
+    render(){
+        return(
+            <div>
+                controls
+            </div>
+        )
+    }
+}
+
+export default TheaterControls
