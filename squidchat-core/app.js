06c94762428d2656d070cb6ea73987c98f6f2c61
ddcjkinghorn <john.kinghorn@coxautoinc.com>
2018-10-09 15:51:55 -0400

    update

--- squidchat-core/app.js
+++ squidchat-core/app.js
@@ -16,6 +16,7 @@ app.use('/upload', express.static(path.join(__dirname, 'squidchat_upload')))
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://squidswap.com:3002");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Credentials', 'true')
     next();
   });
 
