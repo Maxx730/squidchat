01ba6d625e2aed051ce11d22cb8acc2e046a0335
ddcjkinghorn <john.kinghorn@coxautoinc.com>
2018-10-09 15:29:58 -0400

    update:

--- squidchat-core/lib/db/database.js
+++ squidchat-core/lib/db/database.js
@@ -5,7 +5,7 @@ let DatabaseConnection = function(){
     this.conn = mysql.createConnection({
         host:'localhost',
         user:'root',
-        password:'',
+        password:'dRmario',
         database:'squidchat'
     });
 
